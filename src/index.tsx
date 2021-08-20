6808d5f73d49a0f49051934fb6c9d85e2ae651b5
CG <Colorado880@gmail.com>
2021-08-20 01:38:45 -0600

    added smooth scroll polyfill

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/core/styles";
+import smoothscroll from "smoothscroll-polyfill";
 import App from "./App";
 import theme from "./theme";
+
+smoothscroll.polyfill();
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
