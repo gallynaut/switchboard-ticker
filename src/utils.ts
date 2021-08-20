85d3b2930961bbb6660475ab333fc5b66887927f
CG <Colorado880@gmail.com>
2021-08-20 00:08:05 -0600

    price formatting

--- src/utils.ts
+++ src/utils.ts
@@ -3,18 +3,38 @@ import { PublicKey, PublicKeyInitData } from "@solana/web3.js";
 
 export const formatCurrency = (price: number | null | undefined) => {
   if (price) {
-    return currencyFormatter.format(price);
+    if (price < 1) {
+      return currencyFormatter4.format(price);
+    }
+    if (price > 1 && price < 20) {
+      return currencyFormatter3.format(price);
+    }
+    if (price > 1000) {
+      return currencyFormatter0.format(price);
+    }
+    return currencyFormatter2.format(price);
   }
   return "N/A";
 };
 
-const currencyFormatter = new Intl.NumberFormat("en-US", {
+const currencyFormatter0 = new Intl.NumberFormat("en-US", {
   style: "currency",
   currency: "USD",
-
-  // These options are needed to round to whole numbers if that's what you want.
-  // minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
-  // maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+  maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+});
+const currencyFormatter2 = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
+const currencyFormatter3 = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+  maximumFractionDigits: 3,
+});
+const currencyFormatter4 = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+  maximumFractionDigits: 4, // (causes 2500.99 to be printed as $2,501)
 });
 
 export const getCurrentTime = (): string => {
